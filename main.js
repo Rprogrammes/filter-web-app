d737d54786095f3a85356924320907accb0aeaf0
Rprogrammes <150661502+Rprogrammes@users.noreply.github.com>
2023-11-13 14:22:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+function setup(){
+    video = createCapture(VIDEO);
+    video.hide();
+
+    canvas = createCanvas(750, 700);
+    canvas.position(100, 100);
+}
+function draw(){
+
+}
+
+function onClick(){
+    
+}
