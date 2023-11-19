9979e0956ad9502b0f2c3d4b008c6e82f17ef8a3
Rprogrammes <150661502+Rprogrammes@users.noreply.github.com>
2023-11-19 13:31:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+noseX = 0;
+noseY = 0;
+
 function preload(){
 
 }
@@ -5,11 +8,28 @@ function setup(){
     video = createCapture(VIDEO);
     video.hide();
 
-    canvas = createCanvas(750, 700);
-    canvas.position(100, 100);
-}
-function draw(){
+    canvas = createCanvas(700, 400);
+    canvas.position(320,0);
 
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results) 
+{
+	if (results.length > 0) {
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log("noseX = " + noseX + "noseY = " + noseY);
+	}
+}
+
+function modelLoaded() {
+	console.log("Model Loaded");
+}
+
+function draw(){
+    image(video, 0, 0, 700, 400);
 }
 
 function onClick(){
